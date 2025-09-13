c62efcbd0140dde1183e75a83a1a654e8a60de08
aykhanko <kerimliaykhan@gmail.com>
2025-09-13 14:02:59 +0400

    UserStatistic model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class UserStatistic(models.Model):
+    field = models.IntegerField(null=True, blank=True)
+    soil_type = models.CharField(max_length=255, null=True, blank=True)
+    avarage_price = models.IntegerField(null=True, blank=True)
+    climate_class = models.CharField(null=True, blank=True)
+    user = models.CharField(max_length=255, null=True, blank=True)
+    region = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.field
+    
+    class Meta:
+        verbose_name_plural = "User Data"
